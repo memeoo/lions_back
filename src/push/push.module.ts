087ca0f6a81ea6f688141ed6ea29aeb6e46f00fb
memeSong <memehsforever@gmial.com>
2021-06-18 15:08:43 +0900

    error by removing lions.josn

--- src/push/push.module.ts
+++ src/push/push.module.ts
@@ -4,14 +4,14 @@ import { FirebaseAdminModule } from '@aginix/nestjs-firebase-admin'
 import * as admin from 'firebase-admin'
 import { PushController } from './push.controller';
 import { PushService } from './push.service';
-const serviceAccount = require('../../src/lions.json');
+// const serviceAccount = require('../../src/lions.json');
 
 
 @Module({
   imports: [
     FirebaseAdminModule.forRootAsync({
       useFactory: () => ({
-        credential: admin.credential.cert(serviceAccount),
+        // credential: admin.credential.cert(serviceAccount),
         databaseURL: 'https://lions-3cc45.firebaseio.com'
       })
     }),
